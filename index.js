92c1d9f5229ced6fa19528bb872f662af13e5c4b
alvinhui <alvin.hui@qq.com>
2017-12-15 23:45:34 +0800

    refactor: node-pre-gyp

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-var mitie = require('bindings')('mitie');
-
-module.exports = mitie;
+var binary = require('node-pre-gyp');
+var path = require('path');
+var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
+var binding = require(binding_path);
+module.exports = binding;
